1e267a60f2431cc6168e6a4ee8c0d011fe4ce692
nordbjorn <enlightened.dev21@gmail.com>
2025-06-16 20:31:38 +0200

    Add Ark DB entities

--- BTCPayServer.Plugins.ArkPayServer/ArkPluginDbContext.cs
+++ BTCPayServer.Plugins.ArkPayServer/ArkPluginDbContext.cs
@@ -29,6 +29,38 @@ public class ArkPluginDbContext(DbContextOptions<ArkPluginDbContext> options) : 
         base.OnModelCreating(modelBuilder);
         modelBuilder.HasDefaultSchema("BTCPayServer.Plugins.Ark");
 
+        modelBuilder.Entity<ArkStoredTransaction>(entity =>
+        {
+            entity.HasKey(e => e.TransactionId);
+
+            entity.HasMany(e => e.CreatedVtxos)
+                .WithOne(v => v.CreatedByTransaction)
+                .HasForeignKey(v => v.TransactionId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasMany(e => e.SpentVtxos)
+                .WithOne(v => v.SpentByTransaction)
+                .HasForeignKey(v => v.SpentByTransactionId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<VTXO>(entity =>
+        {
+            entity.HasKey(e => new { e.TransactionId, e.TransactionOutputIndex });
+        });
+
+        modelBuilder.Entity<ArkWallet>(entity =>
+        {
+            entity.HasKey(w => w.DescriptorTemplate);
+            entity.HasMany(w => w.Contracts)
+                .WithOne()
+                .HasForeignKey(c => c.DescriptorTemplate);
+        });
+
+        modelBuilder.Entity<ArkWalletContract>(entity =>
+        {
+            entity.HasKey(c => c.Script);
+        });
     }
 }
 
