26544682fcebcbd086954cfb6d15982c690987a8
Andrew Camilleri (Kukks) <evilkukka@gmail.com>
2025-07-23 14:41:09 +0200

    do not cache terms in default operator service

--- NArk/Services/OperatorTermsService.cs
+++ NArk/Services/OperatorTermsService.cs
@@ -5,23 +5,17 @@ using NArk.Wallet;
 
 namespace NArk.Services;
 
-public class OperatorTermsService(ArkService.ArkServiceClient arkClient, ILogger<OperatorTermsService> logger)
+public class OperatorTermsService(
+    ArkService.ArkServiceClient arkClient, 
+    ILogger<OperatorTermsService> logger)
     : IOperatorTermsService
 {
-    private ArkOperatorTerms? _operatorTerms;
-
     public async Task<ArkOperatorTerms> GetOperatorTerms(CancellationToken cancellationToken = default)
     {
-        if (_operatorTerms is not null)
-        {
-            return _operatorTerms;
-        }
-        
         try
         {
             var info = await arkClient.GetInfoAsync(new GetInfoRequest(), cancellationToken: cancellationToken);
             var terms = info.ArkOperatorTerms();
-            _operatorTerms = terms;
             return terms;
         }
         catch (Exception ex)
