1540661dd4f94502fc113cb18e0021b70903d71d
Andrew Camilleri (Kukks) <evilkukka@gmail.com>
2025-07-31 15:58:48 +0200

    make payment links more compat

--- BTCPayServer.Plugins.ArkPayServer/PaymentHandler/ArkadePaymentLinkExtension.cs
+++ BTCPayServer.Plugins.ArkPayServer/PaymentHandler/ArkadePaymentLinkExtension.cs
@@ -9,12 +9,26 @@ namespace BTCPayServer.Plugins.ArkPayServer.PaymentHandler;
 public class ArkadePaymentLinkExtension : IPaymentLinkExtension
 {
     public PaymentMethodId PaymentMethodId { get; } = ArkadePlugin.ArkadePaymentMethodId;
+
     public string? GetPaymentLink(PaymentPrompt prompt, IUrlHelper? urlHelper)
     {
         var onchain = prompt.ParentEntity.GetPaymentPrompt(PaymentTypes.CHAIN.GetPaymentMethodId("BTC"));
         var ln = prompt.ParentEntity.GetPaymentPrompt(PaymentTypes.LN.GetPaymentMethodId("BTC"));
         var lnurl = prompt.ParentEntity.GetPaymentPrompt(PaymentTypes.LNURL.GetPaymentMethodId("BTC"));
-        
+
+        if (string.IsNullOrEmpty(onchain?.Destination) && (ln is not null || lnurl is not null))
+        {
+            return
+                $"lightning:{ln?.Destination ?? lnurl?.Destination}&ark={prompt.Destination}&amount={prompt.Calculate().Due.ToString(CultureInfo.InvariantCulture)}";
+        }
+
+        if (string.IsNullOrEmpty(onchain?.Destination) && ln is  null && lnurl is null)
+        {
+            return
+                $"bitcoin:{prompt.Destination}?amount={prompt.Calculate().Due.ToString(CultureInfo.InvariantCulture)}";
+            
+        }
+
         var res = $"bitcoin:{onchain?.Destination??String.Empty}?amount={prompt.Calculate().Due.ToString(CultureInfo.InvariantCulture)}&ark={prompt.Destination}";
         
         if (ln is not null)
