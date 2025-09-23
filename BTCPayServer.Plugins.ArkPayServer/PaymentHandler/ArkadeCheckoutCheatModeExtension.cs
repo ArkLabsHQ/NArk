6070eff807daa963f998f7204c4c1ba2bdbce0bf
Afshin Arani <afshin@arani.dev>
2025-09-24 00:24:18 +0300

    ArcadeCheckoutCheatModeExtensions: refactor

--- BTCPayServer.Plugins.ArkPayServer/PaymentHandler/ArkadeCheckoutCheatModeExtension.cs
+++ BTCPayServer.Plugins.ArkPayServer/PaymentHandler/ArkadeCheckoutCheatModeExtension.cs
@@ -1,21 +1,13 @@
-using BTCPayServer.Lightning;
 using BTCPayServer.Payments;
 using BTCPayServer.Services;
 using NBitcoin;
-using System;
+using BTCPayServer.Plugins.ArkPayServer.Exceptions;
 using Newtonsoft.Json.Linq;
 
 namespace BTCPayServer.Plugins.ArkPayServer.PaymentHandler
 {
-    public class ArkadeCheckoutCheatModeExtension : ICheckoutCheatModeExtension
+    public class ArkadeCheckoutCheatModeExtension(Cheater cheater) : ICheckoutCheatModeExtension
     {
-        private readonly Cheater _cheater;
-        public ArkadeCheckoutCheatModeExtension(Cheater cheater)
-        {
-            _cheater = cheater;
-        }
-        public BTCPayNetwork Network { get; } 
-
         public bool Handle(PaymentMethodId paymentMethodId) => paymentMethodId == ArkadePlugin.ArkadePaymentMethodId;
 
         public async Task<ICheckoutCheatModeExtension.MineBlockResult> MineBlock(
@@ -53,38 +45,38 @@ namespace BTCPayServer.Plugins.ArkPayServer.PaymentHandler
         {
             var destination = payInvoiceContext.PaymentPrompt.Destination;
             var amt = Money.Coins(payInvoiceContext.Amount).Satoshi;
-            //call:  docker exec -ti arkd ark send --to {destination} --amount {amt}
 
-                // Create process to execute the docker command
-                var process = new System.Diagnostics.Process
-                {
-                    StartInfo = new System.Diagnostics.ProcessStartInfo
-                    {
-                        FileName = "docker",
-                        Arguments = $"exec -i ark ark send --to {destination} --amount {amt}",
-                        RedirectStandardOutput = true,
-                        RedirectStandardError = true,
-                        UseShellExecute = false,
-                        CreateNoWindow = true
-                    }
-                };
-
-                process.Start();
-                var output = await process.StandardOutput.ReadToEndAsync();
-                var error = await process.StandardError.ReadToEndAsync();
-                await process.WaitForExitAsync();
-
-                if (process.ExitCode == 0)
-                {
-                    JObject jObject = JObject.Parse(output);
-
-                    return new ICheckoutCheatModeExtension.PayInvoiceResult(jObject["txid"].Value<string>());
-
-                }
-                
-throw new Exception(error);
-
+            var args = $"ark send --to {destination} --amount {amt} --password secret";
             
+            // Create a process to execute the docker command
+            var process = new System.Diagnostics.Process
+            {
+                StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = "nigiri",
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            
+            var output = await process.StandardOutput.ReadToEndAsync();
+            var error = await process.StandardError.ReadToEndAsync();
+            await process.WaitForExitAsync();
+
+            if (process.ExitCode == 0)
+            {
+                var arkOutput = JObject.Parse(output);
+                var txId = arkOutput.GetValue("txid")?.Value<string>();
+                if (txId is not null)
+                    return new ICheckoutCheatModeExtension.PayInvoiceResult(txId);
+            }
+
+            throw new ExternalProcessFailedException($"docker {args}", error);
         }
     }
 }
